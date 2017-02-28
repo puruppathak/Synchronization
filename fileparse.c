51de6cf84bb899bc2adfc94f21ed8e0bd580ee25
Puru Pathak <puruppathak@gmail.com>
2017-02-28 10:25:20 -0500

    Update fileparse.c

--- fileparse.c
+++ fileparse.c
@@ -7,7 +7,7 @@
 #include <sys/types.h>
 
 
-// Name: Puru Pathak
+// Name: Puru P Pathak
 // Multithreading program using synchronization.
 
 //References: https://computing.llnl.gov/tutorials/pthreads/
