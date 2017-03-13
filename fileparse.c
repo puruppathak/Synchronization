f20b331b3c621d04ecde580311a066608d590d99
Puru Pathak <puruppathak@gmail.com>
2017-03-13 13:34:57 -0400

    Update fileparse.c

--- fileparse.c
+++ fileparse.c
@@ -28,7 +28,7 @@ typedef struct A_Array_struct{                //Associative array structure
 	Pair_variable *pairs;
 }A_Array;
 
-typedef struct Dict_struct{                   //Dictionary structure declared here
+typedef struct Dict_struct{                   //Dictionary structure declared
     int count;
 	A_Array *A_arrays;
 }Dict;
